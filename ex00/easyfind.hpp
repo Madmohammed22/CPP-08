7c1eebe8a21f7065b75e38677fe0c447feccc269
mmad <mohamed.mad1-etu@etu.univh2c.ma>
2024-12-21 22:43:57 +0100

    Updated

---
+++ ex00/easyfind.hpp
@@ -0,0 +1,67 @@
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <iostream>
+#include <unistd.h>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+
+template <class T>
+class Easyfind{
+
+    Easyfind();
+    Easyfind(const Easyfind<T> &Init);
+    Easyfind<T> &operator=(const Easyfind<T> &Init);
+    ~Easyfind();
+
+private:
+    std::vector<T> buffer;
+};
+
+template <class T>
+Easyfind<T>::Easyfind(){
+    std::cout << "[Easyfind] Constructor is called" << std::endl;
+}
+
+template <class T>
+Easyfind<T>::Easyfind(const Easyfind<T> &Init){
+    (void)Init;
+}
+
+template <class T>
+Easyfind<T> &Easyfind<T>::operator=(const Easyfind<T> &Init){
+    (void)Init;
+}
+
+template <class T>
+Easyfind<T>::~Easyfind(){
+    std::cout << "[Easyfind] Distructor is called" << std::endl;
+}
+
+template <typename T> int easyfind(T& buffer, int target){
+    std::vector <int> holder;
+    int size = sizeof(buffer) / sizeof(buffer[0]);
+    for (int i = 0; i < size; i++)
+        holder.push_back(buffer[i]);
+
+    int flag = -1;
+    for (size_t i = 0; i < holder.size(); i++){
+        try
+        {
+            if (target == holder[i])
+                return holder[i];
+            else
+                flag = 0;
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+        
+    }
+    if (flag == 0)
+        throw std::runtime_error("Target not found");
+    return -1;
+}
+#endif
