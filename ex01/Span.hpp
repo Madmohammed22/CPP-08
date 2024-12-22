94d71a56ec3e942313dc84e1d3b9bdba9af718a1
mmad <mohamed.mad1-etu@etu.univh2c.ma>
2024-12-22 12:37:45 +0100

    Updated

--- ex01/Span.hpp
+++ ex01/Span.hpp
@@ -1,73 +1,32 @@
 #ifndef SPAN_HPP
 #define SPAN_HPP
 
-
 #include <iostream>
 #include <unistd.h>
 #include <cstdlib>
 #include <algorithm>
 #include <vector>
+#include <bits/stdc++.h>
 
-template <class T>
 class Span
 {
 
+public:
     Span();
     Span(unsigned int N);
-    Span(const Span<T> &Init);
-    Span<T> &operator=(const Span<T> &Init);
+    Span(const Span &Init);
+    Span &operator=(const Span &Init);
     ~Span();
-    int getSize();
+
 public:
-    T buffer;
-    int size;
+
+    std::vector<int> buffer;
+    unsigned int count;
+    unsigned int reserve;
+    void addNumber(int data);
+    unsigned int longestSpan();
+    unsigned int shortestSpan();
+    void display();
 };
-
-template <class T>
-Span<T>::Span() : size(0)
-{
-    std::cout << "[Span] Constructor is called" << std::endl;
-}
-
-template <class T>
-Span<T>::Span(unsigned int N)
-{
-    this->buffer = new T[N];
-    this->size = this->buffer.size();
-    for (int i = 0; i < this->buffer.size(); i++)
-        this->buffer[i] = 0;
-    
-    std::cout << "[Span] Parametrize constructor is called" << std::endl;
-}
-
-template <class T>
-Span<T>::Span(const Span<T> &Init) : size(Init.size)
-{
-    for (int i = 0; i < this->size; i++)
-        this->buffer[i] = Init.buffer[i];
-}
-
-template <class T>
-Span<T> &Span<T>::operator=(const Span<T> &Init)
-{
-    if (this == &Init)
-        return *this;
-    delete[] this->buffer;
-    this->buffer = new T[Init.getSize()];
-    for (int i = 0; i < this->size; i++)
-        this->buffer[i] = Init.buffer[i];
-}
-
-template <class T> int Span<T>::getSize(){
-    int size = this->buffer.size();
-    return size;
-}
-
-template <class T>
-Span<T>::~Span()
-{
-    delete [] this->buffer;
-    std::cout << "[Span] Distructor is called" << std::endl;
-}
 
 #endif
